1c19486840db291d4c12fc939d43836f881d710b
Nadezhda Orlova <orlova113@rambler.ru>
2020-06-02 22:40:56 +0300

    Задание 3 недели

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,21 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+import locale
+locale.setlocale(locale.LC_ALL, "russian")
+from datetime import datetime, date, timedelta
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    d_today = date.today()
+    print(d_today.strftime('%d.%m.%Y'))
+
+    delta = timedelta(days=1)
+    d_yesterday = d_today - delta
+    print(d_yesterday.strftime('%d.%m.%Y'))
+
+    delta = timedelta(days=30)
+    month_ago = d_today - delta
+    print(month_ago.strftime('%d.%m.%Y'))
 
 
 def str_2_datetime(date_string):
@@ -21,7 +29,9 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    date_string = '01/01/20 12:10:03.234567'
+    date_dt = datetime.strptime(date_string, '%y/%m/%d %H:%M:%S.%f')
+    print(date_dt)
 
 if __name__ == "__main__":
     print_days()
