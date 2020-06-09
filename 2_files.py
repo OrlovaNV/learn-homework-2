ea4e0b1fe6cfefde7c52cd249922cc60d4e9f2b4
Nadezhda Orlova <orlova113@rambler.ru>
2020-06-09 14:53:39 +0300

    Задание 2 недели

--- 2_files.py
+++ 2_files.py
@@ -10,13 +10,23 @@
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
 """
-
+from idlelib.replace import replace
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+
+with open('referat.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
+    print(content)
+    Letters = len(content.upper())
+    word = len(content.split())
+    print(f"Букв в строке: {Letters}")
+    print(f'Слов в строке: {word}')
+
+    change = content.replace('.', '!')
+    print(change)
 
 if __name__ == "__main__":
     main()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -7,13 +7,26 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+spisok = [
+        {'name': 'Masha', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Vasya', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Misha', 'age': 48, 'job': 'Big boss'}, 
+        {'name': 'Petya', 'age': 19, 'job': 'Student'}
+    ]
+ 
+with open('export.csv', 'w', encoding='utf-8', newline='') as f:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(f, fields, delimiter=';')
+    writer.writeheader()
+    for user in spisok:
+        writer.writerow(user)
 
 if __name__ == "__main__":
     main()
